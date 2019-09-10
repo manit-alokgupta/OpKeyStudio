88ffa6a9dc28a519c80f2a52bfc78f8478cd6b13
neonnishant <neonnishant123@gmail.com>
2019-09-10 20:50:47 +0530

    all setup done

---
+++ OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
@@ -0,0 +1,13 @@
+package opkeystudio.parts;
+
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Composite;
+
+public class ProjectExplorerPart {
+	@PostConstruct
+	public void createComposite(Composite parent) {
+	}
+}
