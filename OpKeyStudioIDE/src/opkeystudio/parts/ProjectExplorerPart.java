abb0d30ed5ec284347df23da2c4fb2811c035c39
neonnishant <neonnishant123@gmail.com>
2019-09-11 14:24:20 +0530

    removed unwanted code

--- OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
+++ OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
@@ -1,12 +1,18 @@
 package opkeystudio.parts;
 
-
 import javax.annotation.PostConstruct;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
 
 public class ProjectExplorerPart {
 	@PostConstruct
 	public void createComposite(Composite parent) {
+		
 	}
 }
