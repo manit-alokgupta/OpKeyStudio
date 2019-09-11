d5cabfc197464048c07da7f7d6a50cebeaedfcca
neonnishant <neonnishant123@gmail.com>
2019-09-11 16:15:13 +0530

    updated projects

--- OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
+++ OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
@@ -2,17 +2,12 @@ package opkeystudio.parts;
 
 import javax.annotation.PostConstruct;
 
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.EditorPart;
 
 public class ProjectExplorerPart {
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		
+		TreeView projectTreeView = new TreeView(parent, SWT.BORDER);
 	}
 }
