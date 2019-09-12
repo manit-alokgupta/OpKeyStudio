6a19dd59382b14006699e71b1e16775d66ef6996
neonnishant <neonnishant123@gmail.com>
2019-09-12 20:48:34 +0530

    updated project

--- OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
+++ OpKeyStudioIDE/src/opkeystudio/parts/ProjectExplorerPart.java
@@ -1,15 +1,21 @@
 package opkeystudio.parts;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 import opkeystudio.parts.compositeui.TreeView;
 
 public class ProjectExplorerPart {
+	@Inject
+	MPart mpart;
+	
 	@PostConstruct
 	public void createComposite(Composite parent) {
+		System.out.println(mpart.getLabel());
 		TreeView projectTreeView = new TreeView(parent, SWT.BORDER);
 	}
 }
