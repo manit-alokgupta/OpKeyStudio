9c6ce1b68758fa8005ea28e7436bca31f084cfe5
neonnishant <neonnishant123@gmail.com>
2019-09-12 21:25:47 +0530

    update Project object

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/Projects.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/Projects.java
@@ -1,5 +1,23 @@
 package opkeystudio.opkeystudiocore.core;
 
 public class Projects {
+	private String projectId;
+	private String projectName;
+
+	public String getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
 
 }
