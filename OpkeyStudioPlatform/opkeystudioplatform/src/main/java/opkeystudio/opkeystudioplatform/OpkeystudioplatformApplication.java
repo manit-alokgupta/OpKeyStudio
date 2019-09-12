dd9c225871c223385b96279a605a2d6be35f6c5d
neonnishant <neonnishant123@gmail.com>
2019-09-12 14:00:32 +0530

    commit

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudioplatform/OpkeystudioplatformApplication.java
@@ -0,0 +1,13 @@
+package opkeystudio.opkeystudioplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpkeystudioplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpkeystudioplatformApplication.class, args);
+	}
+
+}
