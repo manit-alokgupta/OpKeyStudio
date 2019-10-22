aee42d1cae10226160e38082fa7ef4876b6a8025
neonnishant <neonnishant123@gmail.com>
2019-10-22 15:26:43 +0530

    added apis

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/apis/ArtificateExporting.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/apis/ArtificateExporting.java
@@ -1,27 +1,58 @@
 package opkeystudio.opkeystudiocore.core.apis;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import opkeystudio.opkeystudiocore.core.communicator.OpKeyApiCommunicator;
+
 public class ArtificateExporting {
-	public void getDetailsOfFolder() {
-
+	public void getFolderDetails(String folderId) throws IOException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("nodeID", folderId);
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/GetFolderDetails", "GET",
+				params, null, null);
 	}
 
-	public void checkForSecuredData() {
-
+	public void checkForSecuredData(String nodeid,String portalType) throws IOException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("strNodeIDs", nodeid);
+		params.put("portalType", portalType);
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/CheckForSecuredData", "GET",
+				params, null, null);
 	}
 
-	public void getSecureDataStatus() {
-
+	public void getSecureDataStatus(String sessionId) throws IOException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("sessionId", sessionId);
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/GetSecuredDataStatus", "GET",
+				params, null, null);
 	}
 
-	public void exportArtificate() {
-
+	public void exportArtificate(String strNodeIDs, String jobType, String sessionID, String SourceLabelID, String SourceProjectPasword) throws IOException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("strNodeIDs", strNodeIDs);
+		params.put("jobType", jobType);
+		params.put("sessionID", sessionID);
+		params.put("SourceLabelID", SourceLabelID);
+		params.put("SourceProjectPasword", SourceProjectPasword);
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/ExportArtifact", "GET",
+				params, null, null);
 	}
 
-	public void getActionLogs() {
-
+	public void getArtifactActionLogs(String sessionId, String jobType, String processtype) throws IOException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("sessionId", sessionId);
+		params.put("jobType", jobType);
+		params.put("processtype", processtype);
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/GetArtifactActionLogs", "GET",
+				params, null, null);
 	}
 
-	public void downloadExportedZip() {
-
+	public void downloadExportedZip(String sessionId) throws IOException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("sessionId", sessionId);
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/DownloadArtifactStuff", "GET",
+				params, null, null);
 	}
 }
--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/apis/ArtificateTreeApi.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/apis/ArtificateTreeApi.java
@@ -17,7 +17,7 @@ public class ArtificateTreeApi {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("moduleType", "AllUnified");
 		params.put("parentID", parentID);
-		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyRest/GetJSTreeNodesLazy", "GET",
+		String retdata = new OpKeyApiCommunicator().sendDataToOpKeyServer("/api/OpKeyStudio/GetJSTreeNodesLazy", "GET",
 				params, null, null);
 		ObjectMapper mapper = Utilities.getInstance().getObjectMapperInstance();
 		CollectionType type = mapper.getTypeFactory().constructCollectionType(List.class, ArtificateTreeNode.class);
