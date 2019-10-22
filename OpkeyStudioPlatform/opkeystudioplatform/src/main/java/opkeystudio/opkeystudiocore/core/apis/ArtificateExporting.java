b7eacb69538dd6dde9ab71ca0c273cf8123d6528
neonnishant <neonnishant123@gmail.com>
2019-10-22 13:07:22 +0530

    added

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/apis/ArtificateExporting.java
@@ -0,0 +1,27 @@
+package opkeystudio.opkeystudiocore.core.apis;
+
+public class ArtificateExporting {
+	public void getDetailsOfFolder() {
+
+	}
+
+	public void checkForSecuredData() {
+
+	}
+
+	public void getSecureDataStatus() {
+
+	}
+
+	public void exportArtificate() {
+
+	}
+
+	public void getActionLogs() {
+
+	}
+
+	public void downloadExportedZip() {
+
+	}
+}
