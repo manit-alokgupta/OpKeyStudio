79739a4a6ee97343c6230e656c57e388242e2d0d
Nikhil Tyagi <nt14434@gmail.com>
2019-10-30 17:47:46 +0530

    issue solved
    now only showing db or sql file in any folder

--- OpKeyStudioIDE/src/opkeystudio/commandhandler/ImportDBCommand.java
+++ OpKeyStudioIDE/src/opkeystudio/commandhandler/ImportDBCommand.java
@@ -1,39 +1,23 @@
 package opkeystudio.commandhandler;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class ImportDBCommand {
 
+	String[] filterExt = { "*.db;*.sql"};
 	@Execute
 	public void execute(Shell shell) {
 
-		FileDialog dialog = new FileDialog(shell);
+		FileDialog dialog = new FileDialog(shell,SWT.OPEN);
+
+		dialog.setFilterExtensions(filterExt);
 		dialog.open();
-//		String[] extension=dialog.getFilterExtensions();
-//		System.out.println(extension);
-
-		String fileName = dialog.getFileName();
-
-//		System.out.println(dialog.getFilterExtensions().toString());
-
-		String extension = "";
-
-		int i = fileName.lastIndexOf('.');
-		if (i > 0) {
-			extension = fileName.substring(i + 1);
-		}
 		
-		System.out.println(extension);
-		if(extension.equals("sql") || extension.equals("sqllite") || extension.equals("db"))
-		{
-			System.out.println(dialog.getFilterPath()+"\\"+dialog.getFileName());
-		}
-		else
-		{
-			System.err.println("File not supported");
-		}
+		System.out.println(dialog.getFilterPath()+"\\"+dialog.getFileName());
+
 
 	}
 
