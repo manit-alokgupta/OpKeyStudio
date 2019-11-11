8c52c1c28d570ce77431cd2796fe1319825d3398
Nikhil Tyagi <nt14434@gmail.com>
2019-11-11 11:26:38 +0530

    Create KeyWordInputArgument.java

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/keywordmanager/dto/KeyWordInputArgument.java
@@ -0,0 +1,21 @@
+package opkeystudio.opkeystudiocore.core.keywordmanager.dto;
+
+public class KeyWordInputArgument {
+	private String name;
+	private String keywordid;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getKeywordid() {
+		return keywordid;
+	}
+	public void setKeywordid(String keywordid) {
+		this.keywordid = keywordid;
+	}
+
+
+}
