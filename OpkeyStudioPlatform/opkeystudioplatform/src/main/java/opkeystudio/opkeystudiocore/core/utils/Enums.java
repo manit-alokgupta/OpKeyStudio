a4cc4f07be797145be24de8cc8834bcd74f3a574
neonnishant <neonnishant123@gmail.com>
2019-12-30 17:48:50 +0530

    Added Enum Files

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/Enums.java
@@ -0,0 +1,9 @@
+package opkeystudio.opkeystudiocore.core.utils;
+
+public class Enums {
+	public enum DataSource {
+		Default, StaticValue, ValueFromInputParameter, ValueFromOutputArgument, ValueFromDataRepository,
+		ValueFromGlobalVariable, ValueFromOpKeyAttachments, ValueFromDataRepositoryFile, ValueFromDynamicObject,
+		ValueFromCollectionModule, ValueFromADGField, ValueFromWebServiceToken
+	};
+}
