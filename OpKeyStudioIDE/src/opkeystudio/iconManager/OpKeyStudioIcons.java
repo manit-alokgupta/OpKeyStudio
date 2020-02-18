fe10f39a0eff624db84da91d394e7b9075ccf212
neonnishant <neonnishant123@gmail.com>
2020-02-18 13:14:56 +0530

    ADDED ICONS ENTRY

--- OpKeyStudioIDE/src/opkeystudio/iconManager/OpKeyStudioIcons.java
+++ OpKeyStudioIDE/src/opkeystudio/iconManager/OpKeyStudioIcons.java
@@ -5,6 +5,7 @@ public class OpKeyStudioIcons {
 	private static String commonIconsDirPath = opkeyDirPath + "commonicons/";
 	private static String artifactIconsDirPath = opkeyDirPath + "artifacts/";
 
+	// Artifact Icons
 	public static String CFL_ICON = artifactIconsDirPath + "cfl.png";
 	public static String DR_ICON = artifactIconsDirPath + "drfile.png";
 	public static String FL_ICON = artifactIconsDirPath + "fl.png";
@@ -13,5 +14,48 @@ public class OpKeyStudioIcons {
 	public static String PLUGIN_ICON = artifactIconsDirPath + "plugin.png";
 	public static String SUITE_ICON = artifactIconsDirPath + "suite.png";
 	public static String TC_ICON = artifactIconsDirPath + "tc.png";
+
+	// Common Icons
+	public static String ADD_COLUMN_ICON = commonIconsDirPath + "add column.png ";
+	public static String ADD_ROW_ICON = commonIconsDirPath + "add row.png";
+	public static String ADD_ICON = commonIconsDirPath + "add.png";
+	public static String ADD_NEW_ICON = commonIconsDirPath + "addnew.png";
+	public static String ADD_NEW_TOOLBAR_ICON = commonIconsDirPath + "addnew_toolbar.png";
+	public static String BACKUP_CREATE_ICON = commonIconsDirPath + "backup create.png";
+	public static String CONTROLLER_ICON = commonIconsDirPath + "controller.png";
+	public static String DELETE_COLUMN_ICON = commonIconsDirPath + "delete column.png";
+	public static String DELETE_ROW_ICON = commonIconsDirPath + "delete row.png";
+	public static String DELETE_ICON = commonIconsDirPath + "delete.png";
+	public static String DELETE_TOOL_ICON = commonIconsDirPath + "delete_tool.png";
+	public static String DESCRIPTION_ICON = commonIconsDirPath + "discreption.png";
+	public static String DR_SMALL_ICON = commonIconsDirPath + "dr.png";
+	public static String ERASER_ICON = commonIconsDirPath + "erasser.png";
+	public static String EXPORT_ICON = commonIconsDirPath + "export.png";
+	public static String GENERATE_DOCUMENT_ICON = commonIconsDirPath + "generate document.png";
+	public static String GLOBAL_VARIABLE_ICON = commonIconsDirPath + "global variable.png";
+	public static String IFRAME_ICON = commonIconsDirPath + "iframe.png";
+	public static String IMPORT_ICON = commonIconsDirPath + "import.png";
+	public static String IMPORT_DB_ICON = commonIconsDirPath + "importdb.png";
+	public static String INPUTDATA_ICON = commonIconsDirPath + "inputdata.png";
+	public static String MAXIMIZE_ICON = commonIconsDirPath + "maximize.png";
+	public static String MINIMIZE_ICON = commonIconsDirPath + "minimize.png";
+	public static String MOVE_LEFT_ICON = commonIconsDirPath + "move left.png";
+	public static String MOVE_RIGHT_ICON = commonIconsDirPath + "move right.png";
+	public static String MOVE_DOWN_ICON = commonIconsDirPath + "movedown.png";
+	public static String MOVE_UP_ICON = commonIconsDirPath + "moveup.png";
+	public static String DR16_ICON = commonIconsDirPath + "or16.png";
+	public static String OUTPUTDATA_ICON = commonIconsDirPath + "output data.png";
+	public static String PROJECT_EXPLORER_ICON = commonIconsDirPath + "project explorar.png";
+	public static String REFRESH_ICON = commonIconsDirPath + "refresh.png";
+	public static String REFRESH_TOOL_ICON = commonIconsDirPath + "refresh_tool.png";
+	public static String RENAME_ICON = commonIconsDirPath + "rename.png";
+	public static String RUN_ICON = commonIconsDirPath + "run.png";
+	public static String SAVE_ICON = commonIconsDirPath + "save.png";
+	public static String SAVE_TOOL_ICON = commonIconsDirPath + "save_tool.png";
+	public static String SCHEDULE_ICON = commonIconsDirPath + "schedule.png";
+	public static String STEP_DETAILS_ICON = commonIconsDirPath + "stepdetail.png";
+	public static String TESTOBJECT_ICON = commonIconsDirPath + "testobject.png";
+	public static String TEXT_KEYWORD_ICON = commonIconsDirPath + "text keyword.png";
+	public static String USER_ICON = commonIconsDirPath + "user.png";
 
 }
