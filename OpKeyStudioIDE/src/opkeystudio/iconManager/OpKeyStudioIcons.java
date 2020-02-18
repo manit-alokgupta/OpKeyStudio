2d8002e083f4df4592f4619d04c90c7ee6a1ffb5
neonnishant <neonnishant123@gmail.com>
2020-02-18 13:02:58 +0530

    ADDED ICON MANAGER

---
+++ OpKeyStudioIDE/src/opkeystudio/iconManager/OpKeyStudioIcons.java
@@ -0,0 +1,17 @@
+package opkeystudio.iconManager;
+
+public class OpKeyStudioIcons {
+	private static String opkeyDirPath = "icons/opkey/";
+	private static String commonIconsDirPath = opkeyDirPath + "commonicons/";
+	private static String artifactIconsDirPath = opkeyDirPath + "artifacts/";
+
+	public static String CFL_ICON = artifactIconsDirPath + "cfl.png";
+	public static String DR_ICON = artifactIconsDirPath + "drfile.png";
+	public static String FL_ICON = artifactIconsDirPath + "fl.png";
+	public static String FOLDER_ICON = artifactIconsDirPath + "folder.png";
+	public static String OR_ICON = artifactIconsDirPath + "or.png";
+	public static String PLUGIN_ICON = artifactIconsDirPath + "plugin.png";
+	public static String SUITE_ICON = artifactIconsDirPath + "suite.png";
+	public static String TC_ICON = artifactIconsDirPath + "tc.png";
+
+}
