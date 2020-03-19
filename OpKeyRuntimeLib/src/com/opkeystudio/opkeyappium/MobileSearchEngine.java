fab7f3d688f3c176a34451ece4f55c5a865c2f1a
manit-alokgupta <53002782+manit-alokgupta@users.noreply.github.com>
2020-03-19 03:31:38 -0700

    update in MobileSearchEngine

--- OpKeyRuntimeLib/src/com/opkeystudio/opkeyappium/MobileSearchEngine.java
+++ OpKeyRuntimeLib/src/com/opkeystudio/opkeyappium/MobileSearchEngine.java
@@ -5,9 +5,11 @@ package com.opkeystudio.opkeyappium;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.openqa.selenium.WebElement;
 
+import com.opkeystudio.runtime.AndroidObject;
 import com.opkeystudio.runtime.ORObject;
 
 import io.appium.java_client.AppiumDriver;
@@ -15,24 +17,49 @@ import io.appium.java_client.MobileElement;
 
 public class MobileSearchEngine {
 
-	ORObject orObj;
-	AppiumDriver<?> appiumDriver;
+	private AppiumDriver<?> appiumDriver;
+	private AndroidObject androidObject;
 
 	@SuppressWarnings("rawtypes")
 	public MobileSearchEngine(AppiumDriver driver, ORObject obj) {
 		this.appiumDriver = driver;
-		this.orObj = obj;
+		this.androidObject = convertORObjectToMobileObject(obj);
 	}
 
 	@SuppressWarnings("unchecked")
 	public WebElement findWebElement() {
 		List<MobileElement> elementsList;
-		Map<String, String> mobileElementProperties = orObj.getAllProperties();
 
 		/**** XPath ****/
-		elementsList = (List<MobileElement>) appiumDriver.findElementsByXPath(mobileElementProperties.get("xpath"));
+		elementsList = (List<MobileElement>) appiumDriver.findElementsByXPath(this.androidObject.getXpath());
 
 		return elementsList.size() > 0 ? (WebElement) elementsList.get(elementsList.size() - 1) : null;
+	}
+
+	private AndroidObject convertORObjectToMobileObject(ORObject orobject) {
+		AndroidObject object = new AndroidObject();
+		Map<String, String> allProperties = orobject.getAllProperties();
+		Set<String> keys = allProperties.keySet();
+
+		for (String key : keys) {
+			String value = orobject.getProperty(key);
+			if (key.equalsIgnoreCase("class")) {
+				object.setClassName(value);
+			}
+			if (key.equalsIgnoreCase("text")) {
+				object.setText(value);
+			}
+			if (key.equalsIgnoreCase("resource-id")) {
+				object.setResourceId(value);
+			}
+			if (key.equalsIgnoreCase("content-desc")) {
+				object.setContentDesc(value);
+			}
+			if (key.equalsIgnoreCase("xpath")) {
+				object.setXpath(value);
+			}
+		}
+		return object;
 	}
 
 }
