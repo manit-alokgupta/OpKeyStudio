b2b2940f70ab5fd5859fe74188aba9abad39d018
neonnishant <neonnishant123@gmail.com>
2020-03-31 16:38:53 +0530

    added package name

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/ArtifactTranspiler.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/ArtifactTranspiler.java
@@ -6,6 +6,9 @@ import opkeystudio.opkeystudiocore.core.apis.dbapi.globalLoader.GlobalLoader;
 import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact;
 
 public class ArtifactTranspiler {
+	private void setPackageProperties(List<Artifact> allArtifacts) {
+	}
+
 	public void transpileAllArtifacts() {
 		List<Artifact> artifacts = GlobalLoader.getInstance().getAllArtifacts();
 		for (Artifact artifact : artifacts) {
