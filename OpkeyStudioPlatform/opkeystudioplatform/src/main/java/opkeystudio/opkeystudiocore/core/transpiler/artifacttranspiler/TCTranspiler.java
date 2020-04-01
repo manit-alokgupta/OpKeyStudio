7c81546a64b28bac0cff6df01c6ab5de47c28730
neonnishant <neonnishant123@gmail.com>
2020-04-01 11:16:13 +0530

    added TCTranspiler

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/artifacttranspiler/TCTranspiler.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/artifacttranspiler/TCTranspiler.java
@@ -1,7 +1,13 @@
 package opkeystudio.opkeystudiocore.core.transpiler.artifacttranspiler;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import opkeystudio.opkeystudiocore.core.apis.dbapi.flow.FlowApi;
 import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact;
 import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact.MODULETYPE;
+import opkeystudio.opkeystudiocore.core.apis.dto.component.FlowStep;
 import opkeystudio.opkeystudiocore.core.utils.Utilities;
 
 public class TCTranspiler extends AbstractTranspiler {
@@ -14,6 +20,17 @@ public class TCTranspiler extends AbstractTranspiler {
 	public void transpile(Artifact artifact) {
 		if (artifact.getFile_type_enum() != MODULETYPE.Flow) {
 			return;
+		}
+		try {
+			List<FlowStep> flowSteps = new FlowApi().getAllFlowSteps(artifact.getId());
+			for (FlowStep flowStep : flowSteps) {
+				if (flowStep.getKeyword() != null) {
+					System.out.println(flowStep.getKeyword().getAssociatedmethod());
+				}
+			}
+		} catch (SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/artifacttranspiler/codeconstruct/TCCodeConstruct.java
@@ -0,0 +1,5 @@
+package opkeystudio.opkeystudiocore.core.transpiler.artifacttranspiler.codeconstruct;
+
+public class TCCodeConstruct {
+
+}
