8413e27463ccf28a2f2313be8cec35db30a47d4d
neonnishant <neonnishant123@gmail.com>
2020-04-04 19:16:05 +0530

    added suite transpiler

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/artifacttranspiler/SuiteTranspiler.java
@@ -0,0 +1,44 @@
+package opkeystudio.opkeystudiocore.core.transpiler.artifacttranspiler;
+
+import java.io.File;
+import java.util.List;
+
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+
+import opkeystudio.opkeystudiocore.core.apis.dbapi.flow.FlowApi;
+import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact;
+import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact.MODULETYPE;
+import opkeystudio.opkeystudiocore.core.apis.dto.component.FlowStep;
+import opkeystudio.opkeystudiocore.core.transpiler.TranspilerUtilities;
+import opkeystudio.opkeystudiocore.core.utils.Utilities;
+
+public class SuiteTranspiler extends AbstractTranspiler {
+	private String newLineChar = "\n";
+
+	public SuiteTranspiler() {
+		setFileExtension(".java");
+		setTranspiledDataFolder(Utilities.getInstance().getTranspiledArtifactsFolder());
+	}
+
+	@Override
+	public void transpile(Artifact artifact) {
+		if (artifact.getFile_type_enum() != MODULETYPE.Suite) {
+			return;
+		}
+		File file = createArtifactFile(artifact);
+		JavaClassSource classSource = getJavaClassOfTestCase(artifact);
+		new TranspilerUtilities().addPackageName(artifact, classSource);
+		new TranspilerUtilities().addDefaultImports(classSource);
+		new TranspilerUtilities().writeCodeToFile(file, classSource);
+	}
+
+	public JavaClassSource getJavaClassOfTestCase(Artifact artifact) {
+		JavaClassSource class1 = Roaster.create(JavaClassSource.class);
+		class1.setName(artifact.getVariableName()).setPublic();
+		List<FlowStep> flowSteps = FlowApi.getInstance().getAllFlowSteps(artifact.getId());
+		String methodBodyCode = "";
+		class1.addMethod().setName("execute").setPublic().setBody(methodBodyCode).addThrows("Exception");
+		return class1;
+	}
+}
