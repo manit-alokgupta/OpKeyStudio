2ee60d5ac7e276cac3589246c9cc841ad5837fcb
neonnishant <neonnishant123@gmail.com>
2020-04-04 12:58:59 +0530

    added FLInputOnbject

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/artifacttranspiler/codeconstruct/FlowInputObject.java
@@ -0,0 +1,95 @@
+package opkeystudio.opkeystudiocore.core.transpiler.artifacttranspiler.codeconstruct;
+
+public class FlowInputObject {
+	private boolean staticValueDataExist;
+	private boolean flowOutputDataExist;
+	private boolean staticObjectDataExist;
+	private boolean globalVariableDataExist;
+	private boolean dataRepositoryColumnDataExist;
+
+	private String staticValueData;
+	private String flowOutputData;
+	private String staticObjectData;
+	private String globalVariableData;
+	private String dataRepositoryColumnData;
+
+	public boolean isStaticValueDataExist() {
+		return staticValueDataExist;
+	}
+
+	public void setStaticValueDataExist(boolean staticValueDataExist) {
+		this.staticValueDataExist = staticValueDataExist;
+	}
+
+	public boolean isFlowOutputDataExist() {
+		return flowOutputDataExist;
+	}
+
+	public void setFlowOutputDataExist(boolean flowOutputDataExist) {
+		this.flowOutputDataExist = flowOutputDataExist;
+	}
+
+	public boolean isStaticObjectDataExist() {
+		return staticObjectDataExist;
+	}
+
+	public void setStaticObjectDataExist(boolean staticObjectDataExist) {
+		this.staticObjectDataExist = staticObjectDataExist;
+	}
+
+	public boolean isDataRepositoryColumnDataExist() {
+		return dataRepositoryColumnDataExist;
+	}
+
+	public void setDataRepositoryColumnDataExist(boolean dataRepositoryColumnDataExist) {
+		this.dataRepositoryColumnDataExist = dataRepositoryColumnDataExist;
+	}
+
+	public boolean isGlobalVariableDataExist() {
+		return globalVariableDataExist;
+	}
+
+	public void setGlobalVariableDataExist(boolean globalVariableDataExist) {
+		this.globalVariableDataExist = globalVariableDataExist;
+	}
+
+	public String getStaticValueData() {
+		return staticValueData;
+	}
+
+	public void setStaticValueData(String staticValueData) {
+		this.staticValueData = staticValueData;
+	}
+
+	public String getFlowOutputData() {
+		return flowOutputData;
+	}
+
+	public void setFlowOutputData(String flowOutputData) {
+		this.flowOutputData = flowOutputData;
+	}
+
+	public String getStaticObjectData() {
+		return staticObjectData;
+	}
+
+	public void setStaticObjectData(String staticObjectData) {
+		this.staticObjectData = staticObjectData;
+	}
+
+	public String getGlobalVariableData() {
+		return globalVariableData;
+	}
+
+	public void setGlobalVariableData(String globalVariableData) {
+		this.globalVariableData = globalVariableData;
+	}
+
+	public String getDataRepositoryColumnData() {
+		return dataRepositoryColumnData;
+	}
+
+	public void setDataRepositoryColumnData(String dataRepositoryColumnData) {
+		this.dataRepositoryColumnData = dataRepositoryColumnData;
+	}
+}
