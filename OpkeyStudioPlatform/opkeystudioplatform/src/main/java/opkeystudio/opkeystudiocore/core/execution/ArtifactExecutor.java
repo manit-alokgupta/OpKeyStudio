9228c0654eb75b5b28e0bee6ce4394ad54a32069
neonnishant <neonnishant123@gmail.com>
2020-04-07 12:39:24 +0530

    added artifactexecutor

---
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/execution/ArtifactExecutor.java
@@ -0,0 +1,13 @@
+package opkeystudio.opkeystudiocore.core.execution;
+
+import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact;
+
+public class ArtifactExecutor {
+	public void execute(Artifact artifact) {
+		initExecute(artifact);
+	}
+
+	private void initExecute(Artifact artifact) {
+
+	}
+}
