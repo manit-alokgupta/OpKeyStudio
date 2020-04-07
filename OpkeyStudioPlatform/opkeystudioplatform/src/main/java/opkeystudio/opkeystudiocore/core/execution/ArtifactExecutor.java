6e488243e1873432a7b6716b12e5a40f809d18fa
neonnishant <neonnishant123@gmail.com>
2020-04-07 17:22:23 +0530

    added session folder

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/execution/ArtifactExecutor.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/execution/ArtifactExecutor.java
@@ -1,6 +1,9 @@
 package opkeystudio.opkeystudiocore.core.execution;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 import opkeystudio.opkeystudiocore.core.apis.dto.component.Artifact;
 import opkeystudio.opkeystudiocore.core.utils.Utilities;
@@ -10,10 +13,15 @@ public class ArtifactExecutor {
 	private String compileFileExt = ".class";
 
 	public void execute(ExecutionSession session) {
-		initExecute(session);
+		try {
+			initExecute(session);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	private void initExecute(ExecutionSession session) {
+	private void initExecute(ExecutionSession session) throws IOException {
 		System.out.println(">> Initiating Execution ");
 		System.out.println(">>Session Id " + session.getSessionId());
 		System.out.println(">>Session Name " + session.getSessionName());
@@ -22,8 +30,9 @@ public class ArtifactExecutor {
 
 		String transpiledFilesDir = Utilities.getInstance().getTranspiledArtifactsFolder();
 
-		String sessionsFolderPath = Utilities.getInstance().getSessionsFolder();
-
+		createExecutionSession(session.getSessionName());
+		String artifactCodesDirPath=getSessionArtifacCodesFolder(session.getSessionName());
+		FileUtils.copyDirectory(new File(transpiledFilesDir), new File(artifactCodesDirPath));
 		String sourceFilePath = transpiledFilesDir + File.separator + artifact.getPackagePath() + File.separator
 				+ artifact.getVariableName() + getSorceFileExt();
 		String compileFilePath = transpiledFilesDir + File.separator + artifact.getPackagePath() + File.separator
@@ -31,7 +40,30 @@ public class ArtifactExecutor {
 		System.out.println(sourceFilePath);
 	}
 
-	
+	private void createExecutionSession(String sessionName) {
+		File file = new File(Utilities.getInstance().getSessionsFolder() + File.separator + sessionName);
+		if (!file.exists()) {
+			file.mkdir();
+		}
+		File file1 = new File(file.getAbsolutePath() + File.separator + "ArtifactCodes");
+		if (!file1.exists()) {
+			file1.mkdir();
+		}
+		File file2 = new File(file.getAbsolutePath() + File.separator + "logs");
+		if (!file2.exists()) {
+			file2.mkdir();
+		}
+	}
+
+	private String getSessionArtifacCodesFolder(String sessionName) {
+		return Utilities.getInstance().getSessionsFolder() + File.separator + sessionName + File.separator
+				+ "ArtifactCodes";
+	}
+
+	private String getSessionLogsFolder(String sessionName) {
+		return Utilities.getInstance().getSessionsFolder() + File.separator + sessionName + File.separator + "logs";
+	}
+
 	public String getSorceFileExt() {
 		return sorceFileExt;
 	}
