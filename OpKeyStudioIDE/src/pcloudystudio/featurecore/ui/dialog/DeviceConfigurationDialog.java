65ba84b9208644b65c17f308412efd85eaecf0d7
manit-alokgupta <53002782+manit-alokgupta@users.noreply.github.com>
2020-04-07 00:21:37 -0700

    DeviceConfigurationDialog update

--- OpKeyStudioIDE/src/pcloudystudio/featurecore/ui/dialog/DeviceConfigurationDialog.java
+++ OpKeyStudioIDE/src/pcloudystudio/featurecore/ui/dialog/DeviceConfigurationDialog.java
@@ -17,6 +17,7 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
@@ -47,9 +48,6 @@ public class DeviceConfigurationDialog extends Dialog {
 	protected Object result;
 	protected Shell shlDeviceConfiguration;
 	private ObjectRepositoryView parentObjectRepositoryView;
-
-	private Composite compositeConfiguration;
-	private Label lblDeviceConfiguration;
 	private Composite compositeConfigurationSettings;
 	private Combo devicesCombo;
 	private Text applicationPathText;
@@ -102,7 +100,7 @@ public class DeviceConfigurationDialog extends Dialog {
 		shlDeviceConfiguration
 		.setImage(ResourceManager.getPluginImage("OpKeyStudio", "icons/pcloudystudio/opkey-16x16.png"));
 		shlDeviceConfiguration.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		shlDeviceConfiguration.setSize(672, 315);
+		shlDeviceConfiguration.setSize(624, 262);
 		shlDeviceConfiguration.setText("Configuration Dashboard");
 
 		Rectangle parentSize = getParent().getBounds();
@@ -115,33 +113,28 @@ public class DeviceConfigurationDialog extends Dialog {
 			MobileCapabilities.getinstance().setMapOfCapabilities(mapOfCapabilities);
 
 		}
-		compositeConfiguration = new Composite(shlDeviceConfiguration, SWT.BORDER);
-		compositeConfiguration.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
-		compositeConfiguration.setBounds(10, 10, 234, 33);
-
-		lblDeviceConfiguration = new Label(compositeConfiguration, SWT.NONE);
-		lblDeviceConfiguration.setText("Device Configuration");
-		lblDeviceConfiguration.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
-		lblDeviceConfiguration.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
-		lblDeviceConfiguration.setBounds(10, 0, 228, 32);
 
 		compositeConfigurationSettings = new Composite(shlDeviceConfiguration, SWT.BORDER);
-		compositeConfigurationSettings.setBounds(10, 54, 646, 166);
+		compositeConfigurationSettings.setBounds(10, 10, 598, 166);
 
 		Label lblDeviceName = new Label(compositeConfigurationSettings, SWT.NONE);
-		lblDeviceName.setBounds(44, 29, 121, 25);
+		lblDeviceName.setBounds(44, 29, 105, 25);
 		lblDeviceName.setText("Device *");
+		lblDeviceName.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
 
 		lblNoDeviceConnected = new Label(compositeConfigurationSettings, SWT.NONE);
 		lblNoDeviceConnected.setVisible(false);
-		lblNoDeviceConnected.setText("No device connected?");
+		lblNoDeviceConnected.setText("No device connected!");
 		lblNoDeviceConnected.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		lblNoDeviceConnected.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
-		lblNoDeviceConnected.setBounds(203, 65, 238, 21);
+		lblNoDeviceConnected.setBounds(155, 62, 230, 21);
 		lblNoDeviceConnected.setVisible(false);
 
 		devicesCombo = new Combo(compositeConfigurationSettings, SWT.READ_ONLY);
-		devicesCombo.setBounds(203, 26, 309, 25);
+		devicesCombo.setBounds(155, 23, 309, 25);
+		GridData gd_combo = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		devicesCombo.setLayoutData(gd_combo);
+		
 		try {
 			devicesList = AndroidDeviceUtil.getAndroidDevices();
 			if (devicesList.size() == 0) {
@@ -159,7 +152,7 @@ public class DeviceConfigurationDialog extends Dialog {
 		}
 
 		Button btnRefresh = new Button(compositeConfigurationSettings, SWT.NONE);
-		btnRefresh.setBounds(529, 25, 75, 33);
+		btnRefresh.setBounds(470, 22, 75, 33);
 		btnRefresh.setText("Refresh");
 
 		devicesCombo.addSelectionListener(new SelectionAdapter() {
@@ -223,20 +216,21 @@ public class DeviceConfigurationDialog extends Dialog {
 		});
 
 		Label lblApplicationFile = new Label(compositeConfigurationSettings, SWT.NONE);
-		lblApplicationFile.setBounds(44, 95, 121, 25);
+		lblApplicationFile.setBounds(44, 95, 105, 25);
 		lblApplicationFile.setText("Application *");
+		lblApplicationFile.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
 
 		applicationPathText = new Text(compositeConfigurationSettings, SWT.BORDER);
-		applicationPathText.setBounds(203, 92, 309, 33);
+		applicationPathText.setBounds(155, 89, 309, 33);
 
 		Button btnBrowseAPK = new Button(compositeConfigurationSettings, SWT.NONE);
-		btnBrowseAPK.setBounds(529, 91, 75, 33);
+		btnBrowseAPK.setBounds(470, 88, 75, 33);
 		btnBrowseAPK.setText("Browse");
 
 		lblDeviceRequiredMessage = new Label(compositeConfigurationSettings, SWT.NONE);
 		lblDeviceRequiredMessage.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		lblDeviceRequiredMessage.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
-		lblDeviceRequiredMessage.setBounds(203, 65, 309, 21);
+		lblDeviceRequiredMessage.setBounds(155, 62, 230, 21);
 		lblDeviceRequiredMessage.setText("Device is required!");
 		lblDeviceRequiredMessage.setVisible(false);
 
@@ -244,7 +238,7 @@ public class DeviceConfigurationDialog extends Dialog {
 		lblApplicationIsRequiredMessage.setText("Application is required!");
 		lblApplicationIsRequiredMessage.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		lblApplicationIsRequiredMessage.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
-		lblApplicationIsRequiredMessage.setBounds(203, 131, 309, 21);
+		lblApplicationIsRequiredMessage.setBounds(155, 128, 230, 21);
 		lblApplicationIsRequiredMessage.setVisible(false);
 
 		Button btnNext = new Button(shlDeviceConfiguration, SWT.NONE);
@@ -285,7 +279,7 @@ public class DeviceConfigurationDialog extends Dialog {
 				}
 			}
 		});
-		btnNext.setBounds(440, 232, 105, 33);
+		btnNext.setBounds(452, 182, 75, 33);
 		btnNext.setText("Next");
 
 		Button btnClose = new Button(shlDeviceConfiguration, SWT.NONE);
@@ -295,7 +289,7 @@ public class DeviceConfigurationDialog extends Dialog {
 				shlDeviceConfiguration.close();
 			}
 		});
-		btnClose.setBounds(551, 232, 105, 33);
+		btnClose.setBounds(533, 182, 75, 33);
 		btnClose.setText("Close");
 		btnBrowseAPK.addSelectionListener(new SelectionAdapter() {
 			@SuppressWarnings("static-access")
