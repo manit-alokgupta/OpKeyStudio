e36e6dca093f9b0c6d8df52021fe52a4c4d9b7f8
neonnishant <neonnishant123@gmail.com>
2020-04-08 19:16:57 +0530

    updated System Plugin Runner

--- OpKeyPluginRunner/SystemPluginRunner/src/main/java/com/opkey/SystemPlugin/OpKeySystemKeywords.java
+++ OpKeyPluginRunner/SystemPluginRunner/src/main/java/com/opkey/SystemPlugin/OpKeySystemKeywords.java
@@ -1,5 +1,480 @@
 package com.opkey.SystemPlugin;
 
 public class OpKeySystemKeywords {
+	public int GetScreenWidth() {
+		return 0;
+	}
 
+	public boolean StringContains() {
+		return false;
+	}
+
+	public boolean VerifyValueInRecordSet() {
+		return false;
+	}
+
+	public String SubString() {
+		return null;
+	}
+
+	public boolean UpdateDataBase() {
+		return false;
+	}
+
+	public boolean AppendTextToFile() {
+		return false;
+	}
+
+	public String LeftTrim() {
+		return null;
+	}
+
+	public boolean CompareString() {
+		return false;
+	}
+
+	public String ConvertDateTimeFormat() {
+		return null;
+	}
+
+	public String FetchValueFromDataBase() {
+		return null;
+	}
+
+	public boolean RecoveryHandlePopup() {
+		return false;
+	}
+
+	public boolean SetOR() {
+		return false;
+	}
+
+	public String Trim() {
+		return null;
+	}
+
+	public boolean IsGreater() {
+		return false;
+	}
+
+	public String ToLower() {
+		return null;
+	}
+
+	public int GetScreenHeight() {
+		return 0;
+	}
+
+	public boolean SetGlobalVariableValue() {
+		return false;
+	}
+
+	public boolean SetCellValueToExcel() {
+		return false;
+	}
+
+	public String Split() {
+		return null;
+	}
+
+	public String GetValueFromCSV() {
+		return null;
+	}
+
+	public String ReplaceSubString() {
+		return null;
+	}
+
+	public boolean ResetORtoOriginal() {
+		return false;
+	}
+
+	public String GetCurrentDateTime() {
+		return null;
+	}
+
+	public String GetFileName() {
+		return null;
+	}
+
+	public String GetSystemIPAddress() {
+		return null;
+	}
+
+	public String RightTrim() {
+		return null;
+	}
+
+	public boolean KillProcess() {
+		return false;
+	}
+
+	public boolean HandleExpectedPopup() {
+		return false;
+	}
+
+	public String GetFileSize() {
+		return null;
+	}
+
+	public String FetchRecordsetFromDataBase() {
+		return null;
+	}
+
+	public boolean SetStepTimeOut() {
+		return false;
+	}
+
+	public String GetFileNameWithoutExtension() {
+		return null;
+	}
+
+	public boolean SetValueToCSV() {
+		return false;
+	}
+
+	public String ReadTextFile() {
+		return null;
+	}
+
+	public boolean DisconnectDataBase() {
+		return false;
+	}
+
+	public int IndexOf() {
+		return 0;
+	}
+
+	public boolean CreateFile() {
+		return false;
+	}
+
+	public String FetchValueFromProcedure() {
+		return null;
+	}
+
+	public boolean KeyPress() {
+		return false;
+	}
+
+	public boolean IsEqual() {
+		return false;
+	}
+
+	public int GetColumnCountofRecordset() {
+		return 0;
+	}
+
+	public boolean ConnectDataBase() {
+		return false;
+	}
+
+	public boolean SendString() {
+		return false;
+	}
+
+	public boolean RestoreStepTimeOut() {
+		return false;
+	}
+
+	public String GetCellValueFromExcel() {
+		return null;
+	}
+
+	public boolean CopyFile() {
+		return false;
+	}
+
+	public String SubtractDateTime() {
+		return null;
+	}
+
+	public int GetRowCountofRecordset() {
+		return 0;
+	}
+
+	public String RemoveDelimiter() {
+		return null;
+	}
+
+	public String ToUpper() {
+		return null;
+	}
+
+	public String FetchAllRecordsFromDatabase() {
+		return null;
+	}
+
+	public boolean IsNotEqual() {
+		return false;
+	}
+
+	public String AddDateTime() {
+		return null;
+	}
+
+	public boolean VerifyValueInProcedure() {
+		return false;
+	}
+
+	public int GetRandomNumber() {
+		return 0;
+	}
+
+	public boolean IsSmaller() {
+		return false;
+	}
+
+	public String ConvertToDateTime() {
+		return null;
+	}
+
+	public String GetFileExtension() {
+		return null;
+	}
+
+	public String GetDayOfWeek() {
+		return null;
+	}
+
+	public String StringConcat() {
+		return null;
+	}
+
+	public String ExecuteQuery() {
+		return null;
+	}
+
+	public Double RoundOff() {
+		return null;
+	}
+
+	public Double EvaluateExpression() {
+		return null;
+	}
+
+	public int GetElementsCountByXpath() {
+		return 0;
+	}
+
+	public boolean GetAllErrorsInSuite() {
+		return false;
+	}
+
+	public String GetExecutionStatus() {
+		return null;
+	}
+
+	public int ConvertToint() {
+		return 0;
+	}
+
+	public Double ConvertToDouble() {
+		return null;
+	}
+
+	public String ConvertToString() {
+		return null;
+	}
+
+	public boolean ConvertToboolean() {
+		return false;
+	}
+
+	public int GetStringLength() {
+		return 0;
+	}
+
+	public boolean StartWindowsService() {
+		return false;
+	}
+
+	public boolean StopWindowsService() {
+		return false;
+	}
+
+	public boolean RestartWindowsService() {
+		return false;
+	}
+
+	public String GetTimeAtTimeZone() {
+		return null;
+	}
+
+	public String GetCurrentTimeZone() {
+		return null;
+	}
+
+	public String ConvertTimeToTimeZone() {
+		return null;
+	}
+
+	public boolean DeleteFile() {
+		return false;
+	}
+
+	public boolean CheckFileExists() {
+		return false;
+	}
+
+	public boolean RenameFile() {
+		return false;
+	}
+
+	public int DateTimeDifference() {
+		return 0;
+	}
+
+	public boolean Isboolean() {
+		return false;
+	}
+
+	public boolean Isint() {
+		return false;
+	}
+
+	public boolean IsDate() {
+		return false;
+	}
+
+	public boolean IsDouble() {
+		return false;
+	}
+
+	public int CountOfSubString() {
+		return 0;
+	}
+
+	public boolean CopyToClipboard() {
+		return false;
+	}
+
+	public String GetSystemProperty() {
+		return null;
+	}
+
+	public String RunCommand() {
+		return null;
+	}
+
+	public String GetStatusOfWindowService() {
+		return null;
+	}
+
+	public boolean SearchTextInFile() {
+		return false;
+	}
+
+	public String GetValueFromJson() {
+		return null;
+	}
+
+	public boolean SetGenericPlugin() {
+		return false;
+	}
+
+	public boolean ResetGenericPluginToDefault() {
+		return false;
+	}
+
+	public String GetFilePath() {
+		return null;
+	}
+
+	public String GetGlobalDRValue() {
+		return null;
+	}
+
+	public boolean SetGlobalDRValue() {
+		return false;
+	}
+
+	public int CreateALMTestRun() {
+		return 0;
+	}
+
+	public int CreateALMTestRunStep() {
+		return 0;
+	}
+
+	public boolean UpdateALMTestRun() {
+		return false;
+	}
+
+	public boolean UploadALMAttachment() {
+		return false;
+	}
+
+	public boolean CheckDuplicacyInCollection() {
+		return false;
+	}
+
+	public boolean CheckDuplicacyInKeyValuePairCollection() {
+		return false;
+	}
+
+	public int GetSizeOfCollection() {
+		return 0;
+	}
+
+	public int GetSizeOfKeyValuePairCollection() {
+		return 0;
+	}
+
+	public Object GetItemFromCollection() {
+		return null;
+	}
+
+	public String GetValueFromKeyValuePair() {
+		return null;
+	}
+
+	public int GetCurrentSessionIndex() {
+		return 0;
+	}
+
+	public boolean SetGlobalVariableCollection() {
+		return false;
+	}
+
+	public String GetKeyFromKeyValuePair() {
+		return null;
+	}
+
+	public boolean SetGlobalVariableKeyValuePairCollection() {
+		return false;
+	}
+
+	public String GetSalesforceAuthorizationToken() {
+		return null;
+	}
+
+	public String GetValueFromXpath() {
+		return null;
+	}
+
+	public String GetAttributeValueFromXpath() {
+		return null;
+	}
+
+	// public ORObject CustomizeObject() {}
+	// public CollectionOfObject AddItemInCollection() {}
+	// public CollectionOfKeyValuePair AddItemInKeyValuePairCollection() {}
+	// public CollectionOfKeyValuePair UpdateItemInKeyValuePairCollection() {}
+	// public CollectionOfKeyValuePair RemoveItemFromKeyValuePairCollection() {}
+	// public KeyValuePair GetItemFromKeyValuePairCollection() {}
+	// public CollectionOfObject GetCollectionOfDelimetedString() {}
+	// public KeyValuePair GetItemFromKeyValuePairCollection(Deprecated)(){}
+	// public CollectionOfObject ReverseCollection() {}
+	// public CollectionOfKeyValuePair ReverseKeyValuePairCollection() {}
+	// public CollectionOfObject RemoveItemFromCollection() {}
+	// public CollectionOfKeyValuePair
+	// RemoveItemFromKeyValuePairCollection(Deprecated)(){}
+	// public CollectionOfObject InsertItemInCollection() {}
+	// public CollectionOfKeyValuePair InsertItemInKeyValuePairCollection() {}
+	// public CollectionOfObject UpdateValueInCollection() {}
+	// public CollectionOfKeyValuePair
+	// UpdateValueInKeyValuePairCollection(Deprecated)(){}
+	// public CollectionOfObject MergeCollection() {}
+	// public CollectionOfKeyValuePair MergeKeyValuePairCollection() {}
 }
