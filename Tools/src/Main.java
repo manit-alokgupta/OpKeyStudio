8cb3a5c7b694a9703a65bc811a67fa1364728949
neonnishant <neonnishant123@gmail.com>
2020-04-09 19:24:13 +0530

    added tools

---
+++ Tools/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	private static String DB_FILEPATH = "E:\\OpKeyEResources\\resources\\GenericDB\\OpKey Generic Keywords.db";
+
+	public static void main(String[] args) {
+		new Converter().convertDBToCode(DB_FILEPATH);
+
+	}
+
+}
