0487dee4a9602693ac5af6044dba336055abe19d
neonnishant <neonnishant123@gmail.com>
2020-04-12 17:12:47 +0530

    added code explorer part

---
+++ OpKeyStudioIDE/src/opkeystudio/parts/part/CodeExplorerPart.java
@@ -0,0 +1,33 @@
+package opkeystudio.parts.part;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.swt.widgets.Composite;
+
+import opkeystudio.featurecore.ide.ui.ui.ArtifactTreeUI;
+
+public class CodeExplorerPart {
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		new ArtifactTreeUI(parent, 0);
+
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+
+	}
+
+	@Focus
+	public void onFocus() {
+
+	}
+
+	@Persist
+	public void save() {
+
+	}
+}
