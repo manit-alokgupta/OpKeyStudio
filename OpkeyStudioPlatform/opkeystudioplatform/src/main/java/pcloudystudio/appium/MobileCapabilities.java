caac29f9050ed09c09828a3fcb2f868ba02d1152
arjunsinsinwar07 <51737223+arjunsinsinwar07@users.noreply.github.com>
2020-04-13 15:23:03 +0530

    changes for DataType in MobileCapabilities class

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/pcloudystudio/appium/MobileCapabilities.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/pcloudystudio/appium/MobileCapabilities.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class MobileCapabilities {
 	private static MobileCapabilities instance;
 	private static LinkedHashMap<String, String> mapOfCapabilities;
+	private static LinkedHashMap<String, String> mapOfCapabilityNameType;
 
 	private MobileCapabilities() {
 	}
@@ -25,17 +26,50 @@ public class MobileCapabilities {
 		MobileCapabilities.mapOfCapabilities = mapOfCapabilities;
 	}
 
+	public static LinkedHashMap<String, String> getMapOfCapabilityNameType() {
+		return mapOfCapabilityNameType;
+	}
+
+	public static void setMapOfCapabilityNameType(LinkedHashMap<String, String> mapOfCapabilityNameType) {
+		MobileCapabilities.mapOfCapabilityNameType = mapOfCapabilityNameType;
+	}
+
 	public static DesiredCapabilities getCapabilities() {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		LinkedHashMap<String, String> caps_map = getMapOfCapabilities();
 		capabilities.setCapability("newCommandTimeout", 0);
 		if (!caps_map.isEmpty()) {
 			caps_map.entrySet().forEach(entry -> {
-				capabilities.setCapability(entry.getKey(), entry.getValue());
+				String capabilityName = entry.getKey();
+				String capabilityType = getCapabilityType(capabilityName);
+				switch (capabilityType) {
+				case "String":
+					capabilities.setCapability(entry.getKey(), entry.getValue());
+					break;
+				case "int":
+					capabilities.setCapability(entry.getKey(), Integer.parseInt(entry.getValue()));
+					break;
+				case "boolean":
+					capabilities.setCapability(entry.getKey(), Boolean.parseBoolean(entry.getValue()));
+					break;
+				default:
+					capabilities.setCapability(entry.getKey(), entry.getValue());
+
+				}
+				// capabilities.setCapability(entry.getKey(), entry.getValue());
 			});
 		}
 
 		return capabilities;
+	}
+
+	public static String getCapabilityType(String capName) {
+		if (mapOfCapabilityNameType != null) {
+			if (mapOfCapabilityNameType.containsKey(capName))
+				return mapOfCapabilityNameType.get(capName);
+
+		}
+		return "String";
 	}
 
 }
