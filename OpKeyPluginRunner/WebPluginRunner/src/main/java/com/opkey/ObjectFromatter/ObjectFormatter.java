e72a7fd0c8caf6449c7ff3d7490a9894679e8477
neonnishant <neonnishant123@gmail.com>
2020-04-13 19:07:43 +0530

    added object formatter

---
+++ OpKeyPluginRunner/WebPluginRunner/src/main/java/com/opkey/ObjectFromatter/ObjectFormatter.java
@@ -0,0 +1,34 @@
+package com.opkey.ObjectFromatter;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.crestech.opkey.plugin.communication.contracts.functioncall.Object;
+import com.crestech.opkey.plugin.communication.contracts.functioncall.Object.Properties.Property;
+import com.crestech.opkey.plugin.webdriver.object.WebDriverObject;
+import com.opkeystudio.runtime.ORObject;
+
+public class ObjectFormatter {
+	public WebDriverObject formatObject(ORObject orobject) {
+		Object _object = convertORObjectToOpKeyObject(orobject);
+		return null;
+	}
+
+	private Object convertORObjectToOpKeyObject(ORObject orobject) {
+		Object object = new Object();
+		object.setLogicalName("Neon");
+
+		Map<String, String> allProperties = orobject.getAllProperties();
+		Set<String> propertyNames = allProperties.keySet();
+		for (String propertyName : propertyNames) {
+			String propertyValue = allProperties.get(propertyName);
+			Property property = new Property();
+			property.setName(propertyName);
+			property.setValue(propertyValue);
+			property.setDataType("String");
+			property.setIsUsed(true);
+			object.getProperties().getProperty().add(property);
+		}
+		return object;
+	}
+}
