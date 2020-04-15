e7d2dbd85cc885c50f1728a9f86883ee8109651c
neonnishant <neonnishant123@gmail.com>
2020-04-15 22:39:48 +0530

    added intellisense

--- OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/customcontrol/codeeditor/ArtifactCodeEditor.java
+++ OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/customcontrol/codeeditor/ArtifactCodeEditor.java
@@ -2,22 +2,27 @@ package opkeystudio.featurecore.ide.ui.customcontrol.codeeditor;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.JTextComponent;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.fife.ui.autocomplete.CompletionProvider;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rsyntaxtextarea.Token;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 import opkeystudio.featurecore.ide.ui.ui.ArtifactCodeView;
@@ -99,6 +104,66 @@ public class ArtifactCodeEditor extends RSyntaxTextArea {
 		RTextScrollPane textScrollPane = new RTextScrollPane(this);
 		mainEditorPanel.add(textScrollPane);
 		frame.add(mainEditorPanel);
+		this.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (saveToggled) {
+					saveToggled = false;
+					return;
+				}
+				try {
+					createIntellisenseDataFromCurrentText();
+				} catch (BadLocationException e1) {
+					e1.printStackTrace();
+				}
+
+				char keyChar = e.getKeyChar();
+				if (keyChar == '.') {
+					Token lastToken = getRecentToken();
+					String tokenData = lastToken.getLexeme();
+					VariableToken varToken = ArtifactCodeCompletionProvider.getInstance(getCodeFunctionView())
+							.findVariableToken(tokenData);
+					if (varToken != null) {
+						tokenData = varToken.getClassName();
+					}
+					AutoCompleteToken autocompletetoken = ArtifactCodeCompletionProvider.getInstance(getCodeFunctionView())
+							.findAutoCompleteToken(tokenData);
+					if (autocompletetoken != null) {
+						JavaCompletionProvider provider = ArtifactCodeCompletionProvider.getInstance(getCodeFunctionView())
+								.getClassMethodsCompletionProvider(autocompletetoken);
+						autoCompletion.setCompletionProvider(provider);
+						autoCompletion.doCompletion();
+					}
+				}
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						
+					}
+				});
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {
+					System.out.println("Saving");
+					saveToggled = true;
+					Display.getDefault().asyncExec(new Runnable() {
+
+						@Override
+						public void run() {
+
+						}
+					});
+					return;
+				}
+			}
+		});
 	}
 
 	private void initWithoutIntellisense(Frame frame) {
@@ -113,6 +178,65 @@ public class ArtifactCodeEditor extends RSyntaxTextArea {
 		frame.add(mainEditorPanel);
 	}
 
+	private void createIntellisenseDataFromCurrentText() throws BadLocationException {
+		try {
+			int lineCount = this.getLineCount();
+			for (int i = 0; i < lineCount; i++) {
+				Token token = this.getTokenListForLine(i);
+
+				List<Token> lineTokens = new ArrayList<Token>();
+				if (token != null) {
+					while (token.getNextToken() != null) {
+						String tokenText = token.getLexeme();
+						if (tokenText != null) {
+							if (!tokenText.trim().isEmpty()) {
+								lineTokens.add(token);
+							}
+						}
+						token = token.getNextToken();
+					}
+				}
+				parseTokens(lineTokens);
+			}
+		} catch (Error e) {
+			// TODO: handle exception
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private Token getRecentToken() {
+		int caretLineNumber = getCaretLineNumber();
+		Token tokens = getTokenListFor(caretLineNumber, getCaretPosition());
+		List<Token> alltokens = new ArrayList<Token>();
+		while (tokens.getNextToken() != null) {
+			String tokenData = tokens.getLexeme().trim();
+			if (!tokenData.isEmpty()) {
+				if (!tokenData.equals("(") && !tokenData.equals(")")) {
+					alltokens.add(tokens);
+				}
+			}
+			tokens = tokens.getNextToken();
+		}
+		Token lastToken = alltokens.get(alltokens.size() - 1);
+		return lastToken;
+	}
+
+	private void parseTokens(List<Token> lineTokens) {
+		for (int i = 0; i < lineTokens.size(); i++) {
+			Token token = lineTokens.get(i);
+			if (token.getLexeme().equals("=")) {
+				String varName = lineTokens.get(i - 1).getLexeme();
+				String varClassName = "";
+				if (lineTokens.get(i - 2) != null) {
+					varClassName = lineTokens.get(i - 2).getLexeme();
+				}
+				VariableToken varToken = new VariableToken(varName, varClassName);
+				ArtifactCodeCompletionProvider.getInstance(getCodeFunctionView()).addVariableToken(varToken);
+				ArtifactCodeCompletionProvider.getInstance(getCodeFunctionView()).addBasicCompletion(varName);
+			}
+		}
+	}
 	public Artifact getArtifact() {
 		return artifact;
 	}
