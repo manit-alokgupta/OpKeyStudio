d9300bc6fea60d775d75e7043ca2a8a784393335
manit-alokgupta <53002782+manit-alokgupta@users.noreply.github.com>
2020-04-15 22:58:58 -0700

    allObjectsCheckboxTreeViewer ui update for elements in hierarchy

--- OpKeyStudioIDE/src/pcloudystudio/objectspy/element/tree/MobileElementLabelProvider.java
+++ OpKeyStudioIDE/src/pcloudystudio/objectspy/element/tree/MobileElementLabelProvider.java
@@ -3,17 +3,11 @@ package pcloudystudio.objectspy.element.tree;
 // Created by Alok Gupta on 20/02/2020.
 // Copyright © 2020 SSTS Inc. All rights reserved.
 
-import org.eclipse.jface.resource.FontDescriptor;
-import org.eclipse.jface.viewers.StyledString;
-import org.eclipse.swt.graphics.Device;
-import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.TextStyle;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.wb.swt.ResourceManager;
 
 import pcloudystudio.objectspy.element.TreeMobileElement;
 import pcloudystudio.objectspy.element.impl.providers.TypeCheckedStyleTreeCellLabelProvider;
-import pcloudystudio.objectspy.resources.image.ImageManager;
 
 public class MobileElementLabelProvider extends TypeCheckedStyleTreeCellLabelProvider<TreeMobileElement> {
 	public MobileElementLabelProvider() {
@@ -25,7 +19,7 @@ public class MobileElementLabelProvider extends TypeCheckedStyleTreeCellLabelPro
 	}
 
 	protected Image getImage(final TreeMobileElement element) {
-		return ImageManager.getImage("pcloudystudio/start_spy.png");
+		return ResourceManager.getPluginImage("OpKeyStudio", "icons/pcloudystudio/test_object-16x16.png");
 	}
 
 	protected String getElementToolTipText(final TreeMobileElement element) {
@@ -34,22 +28,5 @@ public class MobileElementLabelProvider extends TypeCheckedStyleTreeCellLabelPro
 
 	protected String getText(final TreeMobileElement element) {
 		return element.getName();
-	}
-	
-	@SuppressWarnings("unused")
-	private class BoldStyler extends StyledString.Styler {
-		private Font currentFont;
-
-		private BoldStyler(final Font font) {
-			this.currentFont = font;
-		}
-
-		public void applyStyles(final TextStyle textStyle) {
-			final FontDescriptor boldDescriptor = FontDescriptor.createFrom(this.currentFont.getFontData()[0])
-					.setStyle(1);
-			final Font boldFont = boldDescriptor.createFont((Device) Display.getCurrent());
-			// textStyle.foreground = ColorUtil.getTextSuccessfulColor();
-			textStyle.font = boldFont;
-		}
 	}
 }
