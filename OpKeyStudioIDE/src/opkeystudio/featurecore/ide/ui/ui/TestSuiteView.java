16628ff481a9ac06cff59fb78b322a17f8221de5
neonnishant <neonnishant123@gmail.com>
2020-04-20 17:17:49 +0530

    Fixed Execution Wizard ui issues in Test Suite

--- OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/ui/TestSuiteView.java
+++ OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/ui/TestSuiteView.java
@@ -290,7 +290,6 @@ public class TestSuiteView extends Composite {
 
 	private void openExecutionWizard() {
 		ExecutionWizardDialog executionWizard = new ExecutionWizardDialog(getShell(), this);
-		executionWizard.createDialogArea(getParent());
 		executionWizard.open();
 	}
 	
