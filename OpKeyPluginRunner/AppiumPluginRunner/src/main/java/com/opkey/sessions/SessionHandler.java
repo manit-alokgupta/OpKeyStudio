0563527a739910146356ac37308b7b23c3479879
neonnishant <neonnishant123@gmail.com>
2020-04-22 16:26:33 +0530

    removed unwanted code

--- OpKeyPluginRunner/AppiumPluginRunner/src/main/java/com/opkey/sessions/SessionHandler.java
+++ OpKeyPluginRunner/AppiumPluginRunner/src/main/java/com/opkey/sessions/SessionHandler.java
@@ -6,12 +6,7 @@ import com.opkeystudio.core.sessioninterfaces.ExecutionSession;
 public class SessionHandler implements ExecutionSession {
 
 	public void afterSessionEnds() {
-		try {
-			new Browser().Method_CloseAllBrowsers();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 
 	public void beforeSessionStart() {
