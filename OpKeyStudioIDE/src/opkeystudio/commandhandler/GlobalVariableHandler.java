32ff8f2862e84967613f88d1df92dd8ac5c4a17e
manit-alokgupta <alok.gupta@sstsinc.com>
2020-04-23 18:01:54 +0530

    fixed https://myopkey.atlassian.net/browse/SAS-32874

--- OpKeyStudioIDE/src/opkeystudio/commandhandler/GlobalVariableHandler.java
+++ OpKeyStudioIDE/src/opkeystudio/commandhandler/GlobalVariableHandler.java
@@ -1,11 +1,12 @@
-
 package opkeystudio.commandhandler;
 
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 
 import opkeystudio.featurecore.ide.ui.ui.GlobalVariableDialog;
+import opkeystudio.opkeystudiocore.core.repositories.repository.ServiceRepository;
 
 public class GlobalVariableHandler {
 	@Execute
@@ -14,4 +15,10 @@ public class GlobalVariableHandler {
 		gdialog.open();
 	}
 
+	@CanExecute
+	public boolean canExecute() {
+		if (ServiceRepository.getInstance().getDefaultProject() != null)
+			return true;
+		return false;
+	}
 }
