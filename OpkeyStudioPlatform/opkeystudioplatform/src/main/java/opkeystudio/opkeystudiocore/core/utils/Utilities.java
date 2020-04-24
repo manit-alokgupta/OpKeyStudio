b6a5bfe1892cf5152ec1d6d0198bec86e477f734
Anshuman Chatterjee <anshuman.chatterji@gmail.com>
2020-04-24 13:17:23 +0530

    changes in path

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/Utilities.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/Utilities.java
@@ -33,8 +33,8 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 public class Utilities {
 	private static Utilities util;
 	private String defaultInstallDir;
-	private String localResourcesDirPath = "C:\\Users\\neon.nishant\\Desktop\\OpKeyStudioEclipse\\trunk\\OpKeyStudioIDE";
-//	private String localResourcesDirPath = "C:/Users/anshuman.chatterjee/Documents/GitHub/OpKeyStudio/OpKeyStudioIDE";
+//	private String localResourcesDirPath = "C:\\Users\\neon.nishant\\Desktop\\OpKeyStudioEclipse\\trunk\\OpKeyStudioIDE";
+	private String localResourcesDirPath = "C:/Users/anshuman.chatterjee/Documents/GitHub/OpKeyStudio/OpKeyStudioIDE";
 //	private String localResourcesDirPath = "D:\\GitHubProjects\\OpKeyStudio\\OpKeyStudioIDE";
 	private boolean developerMode = true;
 
