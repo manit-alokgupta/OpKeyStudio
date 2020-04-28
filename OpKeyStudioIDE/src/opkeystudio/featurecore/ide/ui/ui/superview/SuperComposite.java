a8e1db5bfe38eeb30c3702d3ab4eed66e8bab36e
neonnishant <neonnishant123@gmail.com>
2020-04-28 13:48:54 +0530

    added SuperComposite

--- OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/ui/superview/SuperComposite.java
+++ OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/ui/superview/SuperComposite.java
@@ -1,11 +1,24 @@
 package opkeystudio.featurecore.ide.ui.ui.superview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.swt.widgets.Composite;
 
+import opkeystudio.featurecore.ide.ui.ui.superview.events.OpKeyGlobalListener;
+
 public class SuperComposite extends Composite {
+	private List<OpKeyGlobalListener> listeners = new ArrayList<>();
 
 	public SuperComposite(Composite parent, int style) {
 		super(parent, style);
 	}
 
+	public void addMyEventListener(OpKeyGlobalListener listener) {
+		listeners.add(listener);
+	}
+
+	public void removeMyEventListener(OpKeyGlobalListener listener) {
+		listeners.remove(listener);
+	}
 }
