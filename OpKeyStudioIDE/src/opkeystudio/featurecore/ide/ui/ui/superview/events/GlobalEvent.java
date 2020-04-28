50ad65f306ff0bae592014c0378bbd46650aa2de
neonnishant <neonnishant123@gmail.com>
2020-04-28 13:32:02 +0530

    added OpKeyGlobalListener

---
+++ OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/ui/superview/events/GlobalEvent.java
@@ -0,0 +1,11 @@
+package opkeystudio.featurecore.ide.ui.ui.superview.events;
+
+import java.util.EventObject;
+
+public class GlobalEvent extends EventObject {
+
+	public GlobalEvent(Object source) {
+		super(source);
+	}
+
+}
