28beb6779de153d4d4eb21846328babdbcc1e690
Anshuman Chatterjee <anshuman.chatterji@gmail.com>
2020-04-29 13:58:01 +0530

    Added object & data parameters in appium plugin runner

--- OpKeyPluginRunner/AppiumPluginRunner/src/main/java/com/opkey/OpKeyGenericPlugin/ReportHelper.java
+++ OpKeyPluginRunner/AppiumPluginRunner/src/main/java/com/opkey/OpKeyGenericPlugin/ReportHelper.java
@@ -3,7 +3,11 @@ package com.opkey.OpKeyGenericPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.crestech.opkey.plugin.communication.contracts.functioncall.FunctionCall;
+import com.crestech.opkey.plugin.communication.contracts.functioncall.FunctionCall.DataArguments.DataArgument;
+import com.crestech.opkey.plugin.communication.contracts.functioncall.FunctionCall.ObjectArguments.ObjectArgument;
 import com.crestech.opkey.plugin.communication.contracts.functionresult.FunctionResult;
+import com.crestech.opkey.plugin.contexts.Context;
 import com.ssts.reporting.ReportBuilder;
 import com.ssts.reporting.Status;
 
@@ -12,6 +16,15 @@ public class ReportHelper {
 	public static void addReportStep(String methodName, FunctionResult functionResult) {
 		Status status = Status.valueOf(functionResult.getStatus().toUpperCase());
 		List<String> parameterList = new ArrayList<String>();
+
+		FunctionCall fc = Context.current().getFunctionCall();
+		for (ObjectArgument oa : fc.getObjectArguments().getObjectArgument()) {
+			parameterList.add(oa.getObject().getLogicalName());
+		}
+
+		for (DataArgument oa : fc.getDataArguments().getDataArgument()) {
+			parameterList.add(oa.getArgumentName() + ":" + oa.getValue());
+		}
 
 		ReportBuilder.get().addStep(methodName, parameterList.toArray(new String[parameterList.size()]), status,
 				functionResult.getOutput());
