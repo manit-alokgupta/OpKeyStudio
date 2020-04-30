4ba36b6d7e4b78cac34baf104bef97dd53c52583
neonnishant <neonnishant123@gmail.com>
2020-04-30 19:48:06 +0530

    added Transpiled class intellisense

--- OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/customcontrol/codeeditor/intellisense/GenericEditorIntellisense.java
+++ OpKeyStudioIDE/src/opkeystudio/featurecore/ide/ui/customcontrol/codeeditor/intellisense/GenericEditorIntellisense.java
@@ -1,9 +1,32 @@
 package opkeystudio.featurecore.ide.ui.customcontrol.codeeditor.intellisense;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DirectColorModel;
+import java.awt.image.IndexColorModel;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.wb.swt.ResourceManager;
 import org.fife.ui.autocomplete.BasicCompletion;
 import org.fife.ui.autocomplete.ShorthandCompletion;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
 
+import opkeystudio.featurecore.ide.ui.customcontrol.codeeditor.FunctionTypeCompletion;
+import opkeystudio.featurecore.ide.ui.customcontrol.codeeditor.JavaBasicCompletion;
 import opkeystudio.featurecore.ide.ui.customcontrol.codeeditor.JavaCompletionProvider;
+import opkeystudio.featurecore.ide.ui.customcontrol.codeeditor.VariableTypeCompletion;
+import opkeystudio.opkeystudiocore.core.compiler.CompilerUtilities;
+import opkeystudio.opkeystudiocore.core.utils.Utilities;
 
 public class GenericEditorIntellisense extends JavaCompletionProvider {
 
@@ -19,6 +42,7 @@ public class GenericEditorIntellisense extends JavaCompletionProvider {
 
 	private void initIntellisense() {
 		addSimpleKeywords();
+		addOpKeyTranspiledClassInformation();
 	}
 
 	private void addSimpleKeywords() {
@@ -35,4 +59,100 @@ public class GenericEditorIntellisense extends JavaCompletionProvider {
 		this.addCompletion(new ShorthandCompletion(this, "sysout", "System.out.println(", "System.out.println();"));
 		this.addCompletion(new ShorthandCompletion(this, "syserr", "System.err.println(", "System.err.println();"));
 	}
+
+	private void addOpKeyTranspiledClassInformation() {
+		String mainDirPath = Utilities.getInstance().getTranspiledArtifactsFolder();
+		List<File> allFiles = new CompilerUtilities().getAllFiles(new File(mainDirPath), ".java");
+		for (File file : allFiles) {
+			String codeData = Utilities.getInstance().readTextFile(file);
+			JavaClassSource classSource = (JavaClassSource) Roaster.parse(codeData);
+			parseConstructors(classSource);
+		}
+	}
+
+	private void parseConstructors(JavaClassSource javaClassSource) {
+		System.out.println("Constructor " + javaClassSource.getName());
+		String className = javaClassSource.getName();
+		addConstructorTypeBasicCompletion(className, className);
+	}
+	
+
+	private void addConstructorTypeBasicCompletion(String dataToShow, String dataToEnter) {
+		String[] dataArray = dataToShow.split("\\.");
+		String datatoShow = dataArray[dataArray.length - 1];
+		JavaBasicCompletion bc = new JavaBasicCompletion(this, datatoShow);
+		bc.setShortDescription(dataToShow);
+		bc.setTextToEnter(dataToEnter);
+		Image img = ResourceManager.getPluginImage("OpKeyStudio", "icons/intellisense/class.ico");
+		BufferedImage image = convertToAWT(img.getImageData());
+		Icon icon = new ImageIcon(image);
+		bc.setIcon(icon);
+		this.addCompletion(bc);
+	}
+
+	public void addMethodTypeBasicCompletion(String dataToShow, String dataToEnter, String returnType) {
+		FunctionTypeCompletion bc = new FunctionTypeCompletion(this, dataToShow, returnType);
+		bc.setShortDescription(dataToShow);
+		bc.setTextToEnter(dataToEnter);
+		this.addCompletion(bc);
+	}
+
+	public void addFieldTypeBasicCompletion(String dataToShow, String dataToEnter, String type) {
+		VariableTypeCompletion bc = new VariableTypeCompletion(this, dataToShow, type);
+		bc.setShortDescription(dataToShow);
+		bc.setTextToEnter(dataToEnter);
+		this.addCompletion(bc);
+	}
+
+	private BufferedImage convertToAWT(ImageData data) {
+		ColorModel colorModel = null;
+		PaletteData palette = data.palette;
+		if (palette.isDirect) {
+			colorModel = new DirectColorModel(data.depth, palette.redMask, palette.greenMask, palette.blueMask);
+			BufferedImage bufferedImage = new BufferedImage(colorModel,
+					colorModel.createCompatibleWritableRaster(data.width, data.height), false, null);
+			WritableRaster raster = bufferedImage.getRaster();
+			int[] pixelArray = new int[3];
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					int pixel = data.getPixel(x, y);
+					RGB rgb = palette.getRGB(pixel);
+					pixelArray[0] = rgb.red;
+					pixelArray[1] = rgb.green;
+					pixelArray[2] = rgb.blue;
+					raster.setPixels(x, y, 1, 1, pixelArray);
+				}
+			}
+			return bufferedImage;
+		} else {
+			RGB[] rgbs = palette.getRGBs();
+			byte[] red = new byte[rgbs.length];
+			byte[] green = new byte[rgbs.length];
+			byte[] blue = new byte[rgbs.length];
+			for (int i = 0; i < rgbs.length; i++) {
+				RGB rgb = rgbs[i];
+				red[i] = (byte) rgb.red;
+				green[i] = (byte) rgb.green;
+				blue[i] = (byte) rgb.blue;
+			}
+			if (data.transparentPixel != -1) {
+				colorModel = new IndexColorModel(data.depth, rgbs.length, red, green, blue, data.transparentPixel);
+			} else {
+				colorModel = new IndexColorModel(data.depth, rgbs.length, red, green, blue);
+			}
+			BufferedImage bufferedImage = new BufferedImage(colorModel,
+					colorModel.createCompatibleWritableRaster(data.width, data.height), false, null);
+			WritableRaster raster = bufferedImage.getRaster();
+			int[] pixelArray = new int[1];
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					int pixel = data.getPixel(x, y);
+					pixelArray[0] = pixel;
+					raster.setPixel(x, y, pixelArray);
+				}
+			}
+			return bufferedImage;
+		}
+	}
+
 }
