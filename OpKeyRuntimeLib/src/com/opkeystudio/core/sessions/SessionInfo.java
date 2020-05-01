57ee717549f76b94ad1376f231b9d63d267b3d00
neonnishant <neonnishant123@gmail.com>
2020-05-01 11:48:03 +0530

    added PluginSettings

--- OpKeyRuntimeLib/src/com/opkeystudio/core/sessions/SessionInfo.java
+++ OpKeyRuntimeLib/src/com/opkeystudio/core/sessions/SessionInfo.java
@@ -1,9 +1,14 @@
 package com.opkeystudio.core.sessions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SessionInfo {
 	private String sessionName;
 	private String defaultPluginLocation;
 	private String reportFilePath;
+
+	private Map<String, String> pluginSettings = new HashMap<String, String>();
 
 	public String getSessionName() {
 		return sessionName;
@@ -27,5 +32,21 @@ public class SessionInfo {
 
 	public void setReportFilePath(String reportFilePath) {
 		this.reportFilePath = reportFilePath;
+	}
+
+	public Map<String, String> getPluginSettings() {
+		return pluginSettings;
+	}
+
+	public void setPluginSettings(Map<String, String> pluginSettings) {
+		this.pluginSettings = pluginSettings;
+	}
+
+	public void addPluginSetting(String key, String value) {
+		getPluginSettings().put(key, value);
+	}
+
+	public String getPluginSetting(String key) {
+		return getPluginSettings().get(key);
 	}
 }
