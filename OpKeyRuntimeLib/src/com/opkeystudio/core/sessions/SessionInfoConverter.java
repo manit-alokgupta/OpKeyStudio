bd35280f3f27f18b833f7093eaec2a66340cc9f0
neonnishant <neonnishant123@gmail.com>
2020-05-04 19:53:18 +0530

    added onverter

--- OpKeyRuntimeLib/src/com/opkeystudio/core/sessions/SessionInfoConverter.java
+++ OpKeyRuntimeLib/src/com/opkeystudio/core/sessions/SessionInfoConverter.java
@@ -12,13 +12,13 @@ public class SessionInfoConverter {
 		Class _class = object.getClass();
 		try {
 			String sessionName = (String) _class.getDeclaredMethod("getSessionName").invoke(object);
-			String defaultPluginLocation = (String) _class.getDeclaredMethod("getDefaultPluginLocation")
-					.invoke(object);
+			String defaultPluginLocation = (String) _class.getDeclaredMethod("getDefaultPluginLocation").invoke(object);
 
 			String reportFilePath = (String) _class.getDeclaredMethod("getReportFilePath").invoke(object);
 
-			MobileDevice mobileDevice = (MobileDevice) _class.getDeclaredMethod("getMobileDevice").invoke(object);
+			Object mobileDeviceObject = _class.getDeclaredMethod("getMobileDevice").invoke(object);
 
+			MobileDevice mobileDevice = convertObjectIntoMobileDevice(mobileDeviceObject);
 			Map<String, String> pluginSettings = (Map<String, String>) _class.getDeclaredMethod("getPluginSettings")
 					.invoke(object);
 
@@ -47,5 +47,33 @@ public class SessionInfoConverter {
 		}
 		return null;
 
+	}
+
+	@SuppressWarnings("unchecked")
+	private static MobileDevice convertObjectIntoMobileDevice(Object object) {
+		MobileDevice device = new MobileDevice();
+		device.getDisplayName();
+		device.getIPAddress();
+		device.getOperatingSystem();
+		device.getSerialNumber();
+		device.getVersion();
+		@SuppressWarnings("rawtypes")
+		Class _class = object.getClass();
+		try {
+			String displayName = (String) _class.getDeclaredMethod("getDisplayName").invoke(object);
+			String ipaddress = (String) _class.getDeclaredMethod("getIPAddress").invoke(object);
+			String os = (String) _class.getDeclaredMethod("getOperatingSystem").invoke(object);
+			String serialNumber = (String) _class.getDeclaredMethod("getSerialNumber").invoke(object);
+			String version = (String) _class.getDeclaredMethod("getVersion").invoke(object);
+			device.setDisplayName(displayName);
+			device.setIPAddress(ipaddress);
+			device.setOperatingSystem(os);
+			device.setSerialNumber(serialNumber);
+			device.setVersion(version);
+			return device;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
 	}
 }
