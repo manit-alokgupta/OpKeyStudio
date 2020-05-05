bbaa0827ba9d9c9fefb3022a8be1b9894f2faf98
Ahmad Sayeed <ahmadsayeed3@gmail.com>
2020-05-05 16:29:55 +0530

    update DataTypeConverter

--- OpKeyPluginRunner/WebPluginRunner/src/main/java/com/opkey/web/DataTypeConverter.java
+++ OpKeyPluginRunner/WebPluginRunner/src/main/java/com/opkey/web/DataTypeConverter.java
@@ -7,9 +7,13 @@ public class DataTypeConverter {
 
 	public static int getInt(String num) {
 		return Integer.parseInt(num);
-	}
+	};
 
 	public static String getMethodName() {
 		return Thread.currentThread().getStackTrace()[2].getMethodName();
+	}
+
+	public static double getDouble(String num) {
+		return Double.parseDouble(num);
 	}
 }
