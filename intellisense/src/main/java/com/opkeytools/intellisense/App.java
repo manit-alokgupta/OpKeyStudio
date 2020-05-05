f82aee58be08a4606faaf8b866d14c37577b306e
neonnishant <neonnishant123@gmail.com>
2020-05-05 19:16:56 +0530

    added intellisense maker

---
+++ intellisense/src/main/java/com/opkeytools/intellisense/App.java
@@ -0,0 +1,34 @@
+package com.opkeytools.intellisense;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import opkeystudio.opkeystudiocore.core.apis.dto.intellisense.ClassIntellisenseDTO;
+import opkeystudio.opkeystudiocore.core.compiler.CompilerUtilities;
+import opkeystudio.opkeystudiocore.core.utils.Utilities;
+
+public class App {
+	private static String EXPORT_DIR_PATH = "C:\\Users\\neon.nishant\\Desktop\\OpKeyStudioEclipse\\trunk\\OpKeyStudioIDE\\resources\\commons\\intellisense\\mainintellisense";
+	private static String File_Name = "mainintellisense.sense";
+
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) throws IOException {
+
+		List<File> files = new CompilerUtilities().getAllFiles(new File(
+				"C:\\Users\\neon.nishant\\Desktop\\OpKeyStudioEclipse\\trunk\\OpKeyStudioIDE\\resources\\libraries\\PluginBase"),
+				".jar");
+		files.addAll(new CompilerUtilities().getAllFiles(new File(
+				"C:\\Users\\neon.nishant\\Desktop\\OpKeyStudioEclipse\\trunk\\OpKeyStudioIDE\\resources\\libraries\\PluginRunner"),
+				".jar"));
+		files.addAll(new CompilerUtilities().getAllFiles(new File(
+				"C:\\Users\\neon.nishant\\Desktop\\OpKeyStudioEclipse\\trunk\\OpKeyStudioIDE\\resources\\libraries\\PluginRunner"),
+				".jar"));
+		List<ClassIntellisenseDTO> classes = new Tools().getAllReflectionsClassesFromJars(files);
+
+		System.out.println("Started DTO Writing to File");
+		Utilities.getInstance().writeXMLSerializedData(new File(EXPORT_DIR_PATH + File.separator + File_Name), classes);
+		System.out.println("Completed DTO Writing to File");
+
+	}
+}
