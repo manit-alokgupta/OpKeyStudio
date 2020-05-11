9a8021ab56e5137a6eabae1ecb23aed08fdaa9c5
neonnishant <neonnishant123@gmail.com>
2020-05-11 13:30:06 +0530

    added variable types

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/OpKeyVariables.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/OpKeyVariables.java
@@ -1,5 +1,7 @@
 package opkeystudio.opkeystudiocore.core.utils;
 
 public class OpKeyVariables {
-
+	public String[] getAllGlobalVariablesType() {
+		return new String[] { "String", "Boolean", "Integer", "Double", "MobileDevice", "MobileApplication" };
+	}
 }
