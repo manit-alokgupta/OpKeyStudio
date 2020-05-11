77131b95090a80397608ea721898a839d1e7d7f3
neonnishant <neonnishant123@gmail.com>
2020-05-11 16:59:37 +0530

    added output param

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/VariableComposer.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/transpiler/VariableComposer.java
@@ -1,5 +1,25 @@
 package opkeystudio.opkeystudiocore.core.transpiler;
 
 public class VariableComposer {
-
+	public String convertOpKeyDataTypeToJavaDataType(String opkeyDataType) {
+		if (opkeyDataType.equals("String")) {
+			return "String";
+		}
+		if (opkeyDataType.equals("Integer")) {
+			return "int";
+		}
+		if (opkeyDataType.equals("Double")) {
+			return "double";
+		}
+		if (opkeyDataType.equals("Boolean")) {
+			return "boolean";
+		}
+		if (opkeyDataType.equals("DateTime")) {
+			return "java.util.Date";
+		}
+		if (opkeyDataType.equals("File")) {
+			return "java.io.File";
+		}
+		return opkeyDataType;
+	}
 }
