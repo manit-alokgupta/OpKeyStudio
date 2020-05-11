fa6d071bdc93efb55d73d018ce401bc5c59cbd9a
neonnishant <neonnishant123@gmail.com>
2020-05-11 13:39:18 +0530

    added variables type

--- OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/OpKeyVariables.java
+++ OpkeyStudioPlatform/opkeystudioplatform/src/main/java/opkeystudio/opkeystudiocore/core/utils/OpKeyVariables.java
@@ -2,6 +2,32 @@ package opkeystudio.opkeystudiocore.core.utils;
 
 public class OpKeyVariables {
 	public String[] getAllGlobalVariablesType() {
-		return new String[] { "String", "Boolean", "Integer", "Double", "MobileDevice", "MobileApplication" };
+		return new String[] { "String", "Boolean", "Integer", "Double", "DateTime", "MobileDevice", "MobileApplication",
+				"File", "List", "SecuredString", "Collections(String)", "Collections(Integer)", "Collections(Double)",
+				"Collections(DateTime)", "Collections(KeyValuePair)", "KeyValuePair" };
+	}
+
+	public String[] getAllFLInputVariablesType() {
+		return new String[] { "String", "Boolean", "Integer", "Double", "DateTime", "MobileDevice", "MobileApplication",
+				"File", "List", "SecuredString", "Collections(String)", "Collections(Integer)", "Collections(Double)",
+				"Collections(DateTime)", "Collections(KeyValuePair)", "KeyValuePair" };
+	}
+
+	public String[] getAllFLOutputVariablesType() {
+		return new String[] { "String", "Boolean", "Integer", "Double", "DateTime", "Collections(String)",
+				"Collections(Integer)", "Collections(Double)", "Collections(DateTime)", "Collections(KeyValuePair)",
+				"KeyValuePair" };
+	}
+
+	public String[] getAllCFLInputVariablesType() {
+		return new String[] { "String", "Boolean", "Integer", "Double", "DateTime", "ORObject", "Collections(String)",
+				"Collections(Integer)", "Collections(Double)", "Collections(DateTime)", "Collections(KeyValuePair)",
+				"KeyValuePair" };
+	}
+
+	public String[] getAllCFLOutputVariablesType() {
+		return new String[] { "String", "Boolean", "Integer", "Double", "DateTime", "Collections(String)",
+				"Collections(Integer)", "Collections(Double)", "Collections(DateTime)", "Collections(KeyValuePair)",
+				"KeyValuePair" };
 	}
 }
